d2f8bc6648cf314ddbe69a73a821f7d1ffb30e4f
Jolybob <ohbelisque@gmail.com>
2026-09-14 01:33:58 +0200

    feat: add JSON import export helpers

---
+++ js/save/ExportManager.js
@@ -0,0 +1,2 @@
+export function downloadJson(filename,data){const blob=new Blob([JSON.stringify(data,null,2)],{type:'application/json'});const url=URL.createObjectURL(blob);const a=document.createElement('a');a.href=url;a.download=filename;a.click();setTimeout(()=>URL.revokeObjectURL(url),500)}
+export async function readJson(file){return JSON.parse(await file.text())}
